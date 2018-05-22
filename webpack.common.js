cd6813ab3eaabaf19c58e05364fca309e75764c7
Joshua Fredrickson <uafredrickson@gmail.com>
2018-05-22 13:11:55 -0700

    added common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,4 +4,40 @@ require('dotenv').config();
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const webpackConfig
+const webpackConfig = module.exports = {};
+
+webpackConfig.entry = `${__dirname}/src/main.js`;
+
+webpackConfig.output = {
+  filename: '[name].[hash].js',
+  path: `${__dirname}/build`,
+  publicPath: process.env.CDN_URL,
+};
+
+webpackConfig.plugin = [
+    new HtmlWebpackPlugin({
+      title: 'Josh Lab 27'
+    }),
+];
+
+webpackConfig.module = {};
+webpackConfig.module.rules = [
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: [
+        'file-loader',
+    ],
+  },
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['env', 'stage-o', 'react'],
+        plugins: ['transform-react-jsx-source'],
+        cascheDirectory: true,
+      },
+    },
+  },
+];

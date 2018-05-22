4e1c5c52141c343ecb5e192a6b85ca3093dd4035
Sarah Geyer <sjgeyer@comcast.net>
2018-05-22 14:16:15 -0700

    search functionality working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import superagent from 'superagent';
 import { render as reactDOMRender } from 'react-dom';
 import './style/main.scss';
 
@@ -10,31 +11,76 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      placeholder: '',
+      subreddit: '',
+      numberOfResults: '',
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubredditChange = this.handleSubredditChange.bind(this);
+    this.handleResultsChange = this.handleResultsChange.bind(this);
+  }
+
+  handleSubredditChange(e) {
+    this.setState({ subreddit: e.target.value });
+  }
+
+  handleResultsChange(e) {
+    this.setState({ numberOfResults: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.subredditSearch(this.state.subreddit, this.state.numberOfResults);
   }
 
   render() {
     return (
-      <div>
-        Hello from Search Form!
-      </div>
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type='text'
+          name='subreddit'
+          placeholder='Search a subreddit'
+          value={ this.state.subreddit }
+          onChange={ this.handleSubredditChange }
+          required
+        />
+        <input
+          type='number'
+          name='numberOfResults'
+          value={ this.state.numberOfResults }
+          placeholder='Desired number of results'
+          onChange={ this.handleResultsChange }
+          min='0'
+          max='100'
+          required
+        />
+        <button type="submit">Search</button>
+      </form>
     );
   }
 }
 
-class SearchResultList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      otherPlaceholder: '',
-    };
-  }
+// {/*<div>*/}
+// {/*<h2 className='error'>*/}
+// {/*{ `"${this.state.pokemonNameError}"` } does not exist. Please make another request.*/}
+// {/*</h2>*/}
+// {/*</div> :*/}
 
+class SearchResultList extends React.Component {
   render() {
     return (
       <div>
-        Hello from Search Result List!
+        {
+          this.props.searchResults ?
+            <div>
+              <h2>Results:</h2>
+
+            </div> :
+            <div>
+              <h2 className='error'>
+                { `"${this.props.error}"` } does not exist
+              </h2>
+            </div>
+        }
       </div>
     );
   }
@@ -44,15 +90,63 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      thirdPlaceholder: '',
+      topics: [],
+      searchError: null,
     };
+    this.subredditSearch = this.subredditSearch.bind(this);
   }
+
+  componentDidUpdate() {
+    console.log('UPDATED STATE', this.state); // eslint-disable-line
+  }
+
+  componentDidMount() {
+    if (localStorage.searchResults) {
+      try {
+        const searchResults = JSON.parse(localStorage.searchResults);
+        return this.setState({ topics: searchResults });
+      } catch (err) {
+        return console.error(err);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  subredditSearch(subreddit, num) {
+    return superagent.get(`https://www.reddit.com/r/${subreddit}/.json?limit=${num - 1}`)
+      .then((response) => {
+        console.log(response.body.data.children); // eslint-disable-line
+        const searchResults = [];
+        response.body.data.children.map((result) => {
+          return searchResults.push({
+            author: result.data.author,
+            title: result.data.title,
+            url: result.data.url,
+            thumbnail: result.data.preview.images[0].source.url,
+          });
+        });
+        try {
+          localStorage.searchResults = JSON.stringify(searchResults);
+          this.setState({ topics: searchResults, searchError: null });
+        } catch (err) {
+          console.error(err); // eslint-disable-line
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ topics: null, searchError: subreddit });
+      }); // eslint-disable-line
+  }
+
   render() {
     return (
       <div>
-        <SearchForm/>
-        <SearchResultList/>
-        <p>Hello from the App Class!</p>
+        <h1>reddit search</h1>
+        <p>In the form below, please enter the subreddit name and number of search
+          results you would like to receive</p>
+        <SearchForm subredditSearch={this.subredditSearch}/>
+        <SearchResultList searchResults={this.state.topics} error={this.state.searchError}/>
       </div>
     );
   }

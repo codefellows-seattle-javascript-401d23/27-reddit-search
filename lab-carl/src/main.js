ef6911aa08c0af50f0d86f7215d297c4847dc766
Carl Olson <colson10@gmail.com>
2018-05-22 21:47:05 -0700

    results in component and added red error border to css

--- lab-carl/src/main.js
+++ lab-carl/src/main.js
@@ -32,15 +32,14 @@ class SearchForm extends React.Component {
     this.props.updateState(this.state.searchFormBoard, this.state.searchFormLimit);
   }
 
-
   render() {
     return (
       <section>
-        <div>
-          <form 
-          className= { this.props.errorCaught ? 'error' : 'search-form' }
-          onSubmit={this.handleSearchFormSubmit
-          }>
+        <form 
+        className= { this.props.errorCaught ? 'error' : 'search-form' }
+        onSubmit={this.handleSearchFormSubmit
+        }>
+          <div>
             <input
               type="text"
               name="boardSearch"
@@ -48,45 +47,55 @@ class SearchForm extends React.Component {
               value={this.state.searchFormBoard}
               onChange={this.handleSearchFormChange}
             />
-          </form>
-        </div>
-        <div>
-          <form onSubmit={this.handleSearchFormSubmit}>
+          </div>
+          <div>
             <input
               type="text"
               name="searchLimit"
               placeholder="Number of Results"
               value={this.state.searchFormLimit}
               onChange={this.handleLimitChange}
+              min='1'
+              max='100'
             />
-          </form>
-        </div>
+          </div>
+            <button type="submit">Search</button>
+        </form>
       </section>
     );
   }
 }
 
-// class SearchResults extends React.Component {
-//   // constructor(props) {
-//   //   super(props);
-//   // }
-  
-//   render() {
-//     if (list !== null)
-//       console.log(this.props.results);
-//     return (
-//       <ul>
-//         { this.props.results.data.children.map((item, index) => {
-//           return (
-//             <li key={index}>
-//               <p>{item.data.permalink}</p>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     );
-//   }
-// }
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.results ?
+          <div>
+            <h2>Search Results</h2>
+            <ul>
+              { this.props.results.data.children.map((item, index) => {
+              return <li key={index}>
+                <a href={item.data.url}>{item.data.title}</a>
+                <p>{item.data.ups}</p>
+                </li>;
+              })}
+            </ul>
+          </div>
+        :
+        undefined
+        }
+        {this.props.error ? 
+        <div>
+          <h2>No results, try again.</h2>
+        </div>
+        :
+        undefined
+        }
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -95,16 +104,32 @@ class App extends React.Component {
       results: '',
       resultsError: '',
     };
-    // this.updateState = this.updateState.bind(this);
-    this.renderSearchResults = this.renderSearchResults.bind(this);
+    this.updateState = this.updateState.bind(this);
   }
 
   componentDidUpdate() {
     console.log('__UPDATE STATE__', this.state);
   }
 
-  componentDidMount() {
-    return superagent.get(`${apiUrl}/travel.json?limit=20`)
+  // componentDidMount() {
+  //   return superagent.get(`${apiUrl}/travel.json?limit=20`)
+  //     .then((response) => {
+  //       this.setState({
+  //         results: response.body,
+  //         resultsError: null,
+  //       });
+  //       this.componentDidUpdate();
+  //     })
+  //     .catch((err) => {
+  //       this.setState({
+  //         results: null,
+  //         resultsError: err,
+  //       });
+  //     });
+  // }
+
+  updateState(board, number) {
+    return superagent.get(`${apiUrl}/${board}.json?limit=${number}`)
       .then((response) => {
         this.setState({
           results: response.body,
@@ -120,59 +145,18 @@ class App extends React.Component {
       });
   }
 
-  // updateState(board, number) {
-  //   return superagent.get(`${apiUrl}/${board}.json?limit=${number}`)
-  //     .then((response) => {
-  //       // console.log(response.body.data.children, 'this is the response');
-  //       this.setState({
-  //         results: response.body,
-  //         resultsError: null,
-  //       });
-  //       this.componentDidUpdate();
-  //     })
-  //     .catch((err) => {
-  //       this.setState({
-  //         results: null,
-  //         resultsError: err,
-  //       });
-  //     });
-  // }
-
-  renderSearchResults() {
-    console.log(this.state.results, 'this is results');
-    if (this.state.results !== null) {
-      return (
-      <ul>
-        { this.state.results.data.children.map((item, index) => {
-          return (
-            <li key={index}>
-              <a href={item.data.url}>{item.data.title}</a>
-              <p>{item.data.ups}</p>
-            </li>
-          );
-        })}
-      </ul>
-      );
-    }
-    return undefined;
-  }
-
   render() {
-    console.log(this.state, 'this is the state');
     return (
       <section>
         <h2>Reddit Search - Lab 27</h2>
-        <SearchForm errorCaught={this.state.resultsError}/>
+        <SearchForm updateState={this.updateState} errorCaught={this.state.resultsError}/>
         <div>
-          {/* <SearchResults results={this.state.results}/> */}
-          {this.state.results && this.renderSearchResults()}
+          <SearchResults results={this.state.results} error={this.state.resultsError}/>
         </div>
       </section>
     );
   }
 }
-// updateState={this.updateState} 
-
 
 const container = document.createElement('div');
 document.body.appendChild(container);

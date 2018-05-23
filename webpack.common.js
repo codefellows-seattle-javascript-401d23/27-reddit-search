4972892da506280675e737a558668ae4d3021131
danielvportlandia <danielshelton90@gmail.com>
2018-05-23 08:10:52 -0700

    able to hit Reddit API.

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+'use strict';
+
+require('dotenv').config();
+// These files won't be transpiled so they must be written in ES5.
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const webpackConfig = module.exports = {};
+
+webpackConfig.entry = `${__dirname}/src/main.js`;
+
+webpackConfig.output = {
+  filename: '[name].[hash].js',
+  path: `${__dirname}/build`,
+  publicPath: process.env.CDN_URL,
+};
+
+webpackConfig.plugins = [
+  new HTMLWebpackPlugin({
+    title: 'Reddit Search Engine',
+  }),
+];
+
+webpackConfig.module = {};
+
+webpackConfig.module.rules = [
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: [
+      'file-loader',
+    ],
+  },
+  {
+    test: /\.js$/,
+    use: {
+      loader: 'babel-loader', // This line is what is transpiling the entire application.
+      options: {
+        presets: ['env', 'stage-0', 'react'],
+        plugins: ['transform-react-jsx-source'],
+        cacheDirectory: true,
+      },
+    },
+  },
+];

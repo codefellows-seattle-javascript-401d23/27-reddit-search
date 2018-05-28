d3a02aeaa15de1d9c55fed5ef97b9ddcf83bf84e
Jennifer Williams <jennifer@squareandplum.com>
2018-05-27 22:25:45 -0700

    render topics from hard-coded reddit board

--- src/main.js
+++ src/main.js
@@ -5,53 +5,63 @@ import superagent from 'superagent';
 
 import '../style/main.scss';
 
-;let searchFormBoard = 'cats';
-let searchFormLimit = 5;
-
-const apiUrl = `http://reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`;
-
-
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      board: 'cats',
+      limit: 5,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBoardChange = this.handleBoardChange.bind(this);
   }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.getTopics(this.state.board, this.state.limit);
+  }
+  handleBoardChange(event) {
+    this.setState({ board: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <h3>Search Form</h3>
-        <form onSubmit={this.handleSubmit()}>
+        <form onSubmit={this.handleSubmit}>
           <input
-          type='text'
-          name='redditBoard'
-          placeholder='Search for a Reddit board'
+            type='text'
+            name='redditBoard'
+            placeholder='Search for a Reddit board'
+            value={this.state.board}
+            onChange={this.handleBoardChange}
+          />
+          <input
+            type='number'
+            name='limit'
           />
           <button type='submit'>Submit</button>
         </form>
       </div>
     );
   }
-
-  handleSubmit() {
-    console.log('handleSubmit');
-  }
 }
 
 class SearchResultList extends React.Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h3>Search Results</h3>
-        <ul>
-          {this.props.topicslist.map((article, index) => {
-            return (
-              <li key={index}>
-                <p>{article}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div>
+      <h3>Search Results</h3>
+      <ul>
+        {this.props.topicsList.map((article, index) => {
+          return (
+            <li key={index}>
+              <p><a href={article.data.url} target='_blank'>{article.data.title}</a></p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
     );
   }
 }
@@ -61,29 +71,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      topics: ['topic1', 'topic2'],
+      topics: [],
     };
+
     this.getTopics = this.getTopics.bind(this);
+    // this.boardSelect = this.boardSelect.bind(this);
   }
 
-  getTopics() {
+  getTopics(board, limit) {
+    const apiUrl = `https://www.reddit.com/r/${board}.json?limit=${limit}`;
     return superagent.get(apiUrl)
       .then((response) => {
         this.setState({
           topics: response.body.data.children,
-        //  topics: ['a', 'b', 'c'],
         });
+        console.log(this.state);
       })
       .catch(console.error);
   }
 
+  // boardSelect(board, limit) {
+  //   const apiUrl = `http://reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`;
+  //   return superagent.get(apiUrl)
+  //     .then()
+  // }
+
   render() {
     return (
-    <section>
-      <h1>Reddit Topics Search</h1>
-      <SearchForm/>
-      <SearchResultList topicslist={ this.state.topics }/>
-    </section>
+      <section>
+        <h1>Reddit Topics Search</h1>
+         <SearchForm getTopics = {this.getTopics} />
+         <SearchResultList topicsList={ this.state.topics }/>
+      </section>
     );
   }
 }
